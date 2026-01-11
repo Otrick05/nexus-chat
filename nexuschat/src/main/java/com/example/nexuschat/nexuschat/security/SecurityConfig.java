08b289da3e61dc488e9e0d388f5462e0bb9e102c
Otrick05 <rdo6686957@gmail.com>
2026-01-10 19:04:02 -0700

    subida del back a la nueva nube (cambio en el cors 4)

--- nexuschat/src/main/java/com/example/nexuschat/nexuschat/security/SecurityConfig.java
+++ nexuschat/src/main/java/com/example/nexuschat/nexuschat/security/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.example.nexuschat.nexuschat.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
@@ -83,6 +84,7 @@ public class SecurityConfig {
                 .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                         .requestMatchers("/api/auth/**", "/ws-chat/**").permitAll()
                         .anyRequest().authenticated())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
